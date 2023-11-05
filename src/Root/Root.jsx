df5394d87c8c7ed87057daefd4050306a9fae4f8
omarjafor <omarjaforchy@gmail.com>
2023-11-05 09:33:11 +0600

    blog website client basic project setup

---
+++ src/Root/Root.jsx
@@ -0,0 +1,11 @@
+
+
+const Root = () => {
+    return (
+        <div>
+            <div> <h1 className="text-3xl font-bold">Hello Root!</h1></div>
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Root/Root";
+
+
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+    },
+]);
+
+export default Router;
