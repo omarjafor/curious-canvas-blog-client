cc0b836627c4e2954fc23dbc150fb598631f887d
omarjafor <omarjaforchy@gmail.com>
2023-11-06 02:35:47 +0600

    newslatter added with relevant message

---
+++ src/Hooks/usePost.jsx
@@ -0,0 +1,15 @@
+import { useQuery } from '@tanstack/react-query'
+
+const usePost = () => {
+
+    const { isPending, data } = useQuery({
+        queryKey: ['blogData'],
+        queryFn: () => fetch('http://localhost:5000/blogs').then(
+            (res) => res.json(),
+        ),
+    })
+
+    return { isPending, data } ;
+};
+
+export default usePost;
